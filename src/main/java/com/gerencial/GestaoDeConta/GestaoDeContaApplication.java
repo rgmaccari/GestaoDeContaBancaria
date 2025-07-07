3c8678b3712e9804f65a0c065a6a0d0ee8d14db1
Rafael Maccari <rafagmaccari@gmail.com>
2025-07-07 17:13:33 -0300

    Commit inicial

---
+++ src/main/java/com/gerencial/GestaoDeConta/GestaoDeContaApplication.java
@@ -0,0 +1,13 @@
+package com.gerencial.GestaoDeConta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeContaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerencial/GestaoDeConta/GestaoDeContaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerencial.GestaoDeConta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
