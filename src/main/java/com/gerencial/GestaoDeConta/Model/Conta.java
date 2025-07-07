fc0b9a279efbb383eb2042196f1b9787ea488d77
Rafael Maccari <rafagmaccari@gmail.com>
2025-07-07 17:59:05 -0300

    Anotação relativa ao Id

--- src/main/java/com/gerencial/GestaoDeConta/Model/Conta.java
+++ src/main/java/com/gerencial/GestaoDeConta/Model/Conta.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Pessoa pessoa;
     private Integer numero;
